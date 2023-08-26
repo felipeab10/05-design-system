f84b8fc6ebbf5d169a60f262f39fac8cddd07835
felipe almeida batista <felipeab10@hotmail.com>
2023-08-26 14:21:57 -0400

    change name of packages for felipeab10-ignite-ui

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -1,4 +1,4 @@
-import { Text, TextProps } from '@ignite-ui/react'
+import { Text, TextProps } from '@felipeab10-ignite-ui/react'
 import type { Meta, StoryObj } from '@storybook/react'
 
 export default {
